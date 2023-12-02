ac9e90046e5cb55960bf1ae5f6dd69c34c186a87
devparbez <devparbez@gmail.com>
2023-12-02 22:19:55 +0600

    animate added

--- script.js
+++ script.js
@@ -4,7 +4,16 @@ let navcloss = document.querySelector('.nav-closs')
 navToggle.addEventListener('click', ()=>{
     nav.classList.toggle('active')
 })
-
 navcloss.addEventListener('click', ()=>{
     nav.classList.remove('active')
+})
+let heroimg = document.querySelector('.hero-pic img')
+window.addEventListener('load', ()=>{
+    heroimg.style.transform = 'translateX(0)'
+    heroimg.style.transitionDuration = '1s'
+})
+let aboutimg = document.querySelector('.about-pic img')
+
+window.addEventListener('load', ()=>{
+    aboutimg.style.transform = 'scale(1)'
 })
